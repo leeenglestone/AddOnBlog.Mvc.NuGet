6808fb975b35930a4ca74987a6652a28dce26728
Lee Englestone <lee.englestone@gmail.com>
2014-07-28 22:43:54 +0100

    TDD started for helper library (serialisation etc)

---
+++ AddOnBlog.Mvc.Tests/PostTests.cs
@@ -0,0 +1,27 @@
+﻿using AddOnBlog.Mvc.Interfaces;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddOnBlog.Mvc.Tests
+{
+    public class PostTests
+    {
+        IBlogRepository _blogRepository;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            // Dependency injection using Ninject
+        }
+
+        [Test]
+        public void Posts_Get_All()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
